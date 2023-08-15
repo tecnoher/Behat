53a7e9287e96024bc52cf21d2751adbc4196634b
Hernan <tecnoher@gmail.com>
2023-08-15 12:12:55 -0300

    add circleCiNode

--- src/Behat/Behat/Output/Node/Printer/JUnit/JUnitScenarioPrinter.php
+++ src/Behat/Behat/Output/Node/Printer/JUnit/JUnitScenarioPrinter.php
@@ -108,7 +108,7 @@ final class JUnitScenarioPrinter
      * @return string
      */
     private function appendLineNumberToPath(string $path, int $lineNumber) {
-        return "$path:$lineNumber";
+        return "$path=$lineNumber";
     }
 
     /**
@@ -129,7 +129,8 @@ final class JUnitScenarioPrinter
      */
     private function addCircleCiAttributes(array $attributes, FeatureNode $feature, ScenarioLikeInterface $scenario) {
         if ($this->circleCiNode !== false) {
-            $attributes['classname'] = "[Node #$this->circleCiNode] " . $feature->getTitle();
+            $attributes['classname'] = $feature->getTitle();
+            $attributes['circleCiNode'] = $this->circleCiNode;
             $attributes['file'] = $this->appendLineNumberToPath($this->convertToRelativePath($feature->getFile()), $scenario->getLine());
             $attributes['line'] = $scenario->getLine();
         }
